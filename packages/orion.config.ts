7afa8411c3eac13abecb9c9765ef150f0848337a
Preston Macy <preston.macy@gmail.com>
2026-01-11 10:08:38 -0500

    set orion config to default for new deployments

--- packages/orion.config.ts
+++ packages/orion.config.ts
@@ -9,81 +9,12 @@ const config = {
   "version": "1.0",
   "name": "orion",
   "defaults": {
-    "maxAge": 300,
+    "maxAge": 60,
     "staleWhileRevalidate": 0,
     "staleIfError": 0
   },
-  "rules": [
-    {
-      "types": [
-        "User"
-      ],
-      "maxAge": 300,
-      "staleWhileRevalidate": 60,
-      "staleIfError": 300,
-      "scope": "private"
-    },
-    {
-      "types": [
-        "Post"
-      ],
-      "maxAge": 300,
-      "staleWhileRevalidate": 60,
-      "staleIfError": 300,
-      "scope": "public"
-    },
-    {
-      "types": [
-        "Comment"
-      ],
-      "maxAge": 300,
-      "staleWhileRevalidate": 60,
-      "staleIfError": 300,
-      "scope": "public"
-    },
-    {
-      "types": [
-        "DatabaseResetResponse"
-      ],
-      "maxAge": 0,
-      "scope": "private",
-      "passthrough": true
-    }
-  ],
-  "invalidations": {
-    "createUser": [
-      "User"
-    ],
-    "createPost": [
-      "Post"
-    ],
-    "createComment": [
-      "Comment"
-    ],
-    "updateUser": [
-      "User"
-    ],
-    "updatePost": [
-      "Post"
-    ],
-    "updateComment": [
-      "Comment"
-    ],
-    "deleteUser": [
-      "User"
-    ],
-    "deletePost": [
-      "Post"
-    ],
-    "deleteComment": [
-      "Comment"
-    ],
-    "reset": [
-      "User",
-      "Post",
-      "Comment"
-    ]
-  }
+  "rules": [],
+  "invalidations": {}
 };
 
 export default config;
